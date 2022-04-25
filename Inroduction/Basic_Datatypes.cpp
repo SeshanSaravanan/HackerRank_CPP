233aee79d0685b07baa8f342dac19534be1dfbb9
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2022-04-25 13:00:24 +0530

    Update Basic_Datatypes.cpp

--- Inroduction/Basic_Datatypes.cpp
+++ Inroduction/Basic_Datatypes.cpp
@@ -38,25 +38,22 @@ Output Format
 Print each element on a new line in the same order it was received as input. Note that the floating point value should be correct up to 3
 decimal places and the double to 9 decimal places. */
 
-
 #include <iostream>
 #include <cstdio>
 using namespace std;
 
-int main() {
+int main() 
+{
     // Complete the code.
-    int a;
-    long b;
-    long long c;
-    char d;
-    float e;
-    double f;    
-    scanf("%d %ld %lld %c %f %lf", &a, &b, &c, &d, &e, &f);
-    printf("%d\n", a);
-    printf("%ld\n", b);
-    printf("%lld\n", c);
-    printf("%c\n", d);
-    printf("%f\n", e);
-    printf("%lf", f);
-    return 0;
+ int a;
+ long long int b;
+ char c;
+ float d;
+ double e;
+
+ scanf("%d %lld %c %f %lf",&a,&b,&c,&d,&e);
+
+ printf("%d\n%lld\n%c\n%f\n%lf",a,b,c,d,e);
+
+return 0;
 }
